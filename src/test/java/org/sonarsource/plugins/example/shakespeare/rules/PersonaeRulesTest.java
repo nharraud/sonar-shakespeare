ce611055847d8ca47e9a77131b9838cd0a7fbd7c
Nicolas Harraudeau <nicolas.harraudeau@gmail.com>
2019-03-31 01:36:42 +0100

    Fix PersonaeRulesTest

--- src/test/java/org/sonarsource/plugins/example/shakespeare/rules/PersonaeRulesTest.java
+++ src/test/java/org/sonarsource/plugins/example/shakespeare/rules/PersonaeRulesTest.java
@@ -26,6 +26,7 @@ import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.batch.sensor.issue.Issue;
 import org.sonar.api.batch.sensor.issue.IssueLocation;
 import org.sonar.sslr.parser.LexerlessGrammar;
+import org.sonarsource.plugins.example.rules.PersonaeCheck;
 import org.sonarsource.plugins.example.shakespeare.ShakespeareParser;
 
 /**
@@ -38,33 +39,36 @@ public class PersonaeRulesTest {
 
   @Test
   public void testCheck() throws IOException {
-    // String program = String.join("\n"
-    // , "This is a title."
-    // , ""
-    // , "Romeo, a young man with a remarkable patience."
-    // , "Juliet, a likewise young woman of remarkable grace."
-    // // , "Ophelia, a remarkable woman much in dispute with Hamlet."
-    // // , "Hamlet, the flatterer of Andersen Insulting A/S."
-    // );
-    // TestInputFileBuilder fileBuilder = TestInputFileBuilder.create("myModule", ".");
+    String program = String.join("\n"
+    , "This is a title."
+    , ""
+    , "Romeo, a young man with a remarkable patience."
+    , "Juliet, a likewise young woman of remarkable grace."
+    , "Ophelia, a remarkable woman much in dispute with Hamlet."
+    , "Hamlet, the flatterer of Andersen Insulting A/S."
+    , "Act I: Hamlet's insults and flattery."
+    , "Scene I: The insulting of Romeo."
+    , "[Enter Hamlet and Romeo]"
+    );
+    TestInputFileBuilder fileBuilder = TestInputFileBuilder.create("myModule", ".");
 
-    // DefaultInputFile inputFile = fileBuilder.setContents(program).setLanguage("shakespeare").build();
+    DefaultInputFile inputFile = fileBuilder.setContents(program).setLanguage("shakespeare").build();
     
-    // SensorContextTester context = SensorContextTester.create(new File("."));
-    // context.fileSystem().add(inputFile);
+    SensorContextTester context = SensorContextTester.create(new File("."));
+    context.fileSystem().add(inputFile);
 
-    // walker.addVisitor(new PersonaeCheck(context, inputFile));
-    // AstNode node = p.parse(inputFile.contents());
-    // walker.walkAndVisit(node);
-    // Collection<Issue> issues = context.allIssues();
-    // assertEquals(issues.size(), 1);
-    // Issue firstIssue = issues.iterator().next();
-    // assertEquals(firstIssue.ruleKey(), PersonaeCheck.KEY);
-    // IssueLocation location = firstIssue.primaryLocation();
-    // TextRange range = location.textRange();
-    // assertEquals(
-    //   new DefaultTextRange(new DefaultTextPointer(3, 0), new DefaultTextPointer(3, 5)),
-    //   range
-    // );
+    walker.addVisitor(new PersonaeCheck(context, inputFile));
+    AstNode node = p.parse(inputFile.contents());
+    walker.walkAndVisit(node);
+    Collection<Issue> issues = context.allIssues();
+    assertEquals(issues.size(), 1);
+    Issue firstIssue = issues.iterator().next();
+    assertEquals(firstIssue.ruleKey(), PersonaeCheck.KEY);
+    IssueLocation location = firstIssue.primaryLocation();
+    TextRange range = location.textRange();
+    assertEquals(
+      new DefaultTextRange(new DefaultTextPointer(3, 0), new DefaultTextPointer(3, 5)),
+      range
+    );
   }
 }
